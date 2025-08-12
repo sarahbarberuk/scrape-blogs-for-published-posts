93811c1f9a65b34d352b8315e1d575a88771e921
sarahbarberuk <sarahbarberuk@gmail.com>
2025-08-12 11:52:12 +0100

    readded config for clients accidentally removed

--- scraper_config.py
+++ scraper_config.py
@@ -1,5 +1,17 @@
 SCRAPER_CONFIGS = {
-    
+    "contentful": {
+        "display_name": "Contentful",
+        "url": "https://www.contentful.com/blog/category/guides/",
+        "post_selector": 'a[class*="blog_entry_card_card"]',
+        "title_selector": 'p[class*="card_with_image_title"]',
+        "date_selector": 'p[class*="card_with_image_description"]',
+        "base_url": "https://www.contentful.com",
+        "needs_click_to_load_more": True,
+        "load_more_selector": 'button:has-text("See more articles")',
+        "load_more_delay": 2000,
+        "wait_until": "domcontentloaded"
+
+    },
     "appsmith": {
         "display_name": "Appsmith",
         "url": "https://www.appsmith.com/blog",
@@ -21,5 +33,29 @@ SCRAPER_CONFIGS = {
         "load_more_selector": 'button:has-text("Load more")',
         "load_more_delay": 2000,
         "wait_until": "domcontentloaded"
+    },
+    "stytch": {
+        "display_name": "Stytch",
+        "url": "https://stytch.com/blog/category/latest/",
+        "post_selector": "main > section:first-of-type a[href^='/blog/']:has(h3)",
+        "title_selector": "main > section:first-of-type a[href^='/blog/'] h3",
+        "date_selector": "main > section:first-of-type a[href^='/blog/'] p",
+        "base_url": "https://www.stytch.com",
+        "needs_click_to_load_more": False,
+        "wait_until": "domcontentloaded"
+    },
+        "akamai": {
+        "display_name": "Akamai",
+        "url": "https://akamai.com/blog/",
+        "post_selector": "div.card.post", 
+        "title_selector": "h3 > a",        
+        "date_selector": "div.post__author",
+        "author_selector": "div.post__author > [href^='/blog?author=']",
+        "base_url": "https://www.akamai.com",
+        "needs_click_to_load_more": True,
+        "load_more_selector": 'div.filter-items__load-more a[rel="next"]',
+        "load_more_delay": 3000,
+        "wait_until": "networkidle",
+        "force_headed": True  
     }
 }
